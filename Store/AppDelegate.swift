1eb6fcaef4007db903b5c30e1fb3339f58fc0bdb
양혜리 <yangyang6424@gmail.com>
2019-05-11 15:26:16 +0900

    :construction: create Project

---
+++ Store/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Store
+//
+//  Created by 양혜리 on 11/05/2019.
+//  Copyright © 2019 hryang. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
